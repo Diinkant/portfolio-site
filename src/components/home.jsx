d607a00a3d4f2726889b7ef8ded5b602b7fa7aa1
Diinkant <diinkant220@gmail.com>
2020-01-08 11:05:23 +1300

    5th commit

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -22,7 +22,7 @@ export default class Home extends Component {
                                     </div>
                                 </div>
                             </li>
-                            <li style={{backgroundImage: 'url(images/img_bg_2.jpg)'}}>
+                            <li style={{backgroundImage: 'url(images/swag.jpg)'}}>
                                 <div className='overlay'/>
                                 <div className='container-fluid'>
                                     <div className='row'>
